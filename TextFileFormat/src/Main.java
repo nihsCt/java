ca89f2a292424a63c2c91002d588d71d96209c83
Nihs Liao <eye_tree@ymail.com>
2018-10-24 23:10:19 +0800

    no message

--- TextFileFormat/src/Main.java
+++ TextFileFormat/src/Main.java
@@ -3,7 +3,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-        String path = "chinese/annotations/bc/cctv/00/test";
+        String path = "chinese/annotations/";
         if(args.length > 0)
             path = args[0];
 
